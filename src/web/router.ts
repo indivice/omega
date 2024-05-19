f66b3ee4dcbb0949b109495930214604a51d03dd
MayukhChakrabortyDX <kismotechindia@gmail.com>
2024-05-19 15:26:16 +0530

    Added native BrowserRouter support for v A13

--- src/web/router.ts
+++ src/web/router.ts
@@ -1,4 +1,4 @@
-import { $switch, $when, Layout } from "../components"
+import { $switch, $when, Content, Layout } from "../components"
 import { Component, State } from "../index"
 
 export class $virtualRouter {
@@ -42,9 +42,78 @@ export class $virtualRouter {
 
 }
 
+export class $browserRouter {
+
+    instance: State<Location> = new State<Location>(window.location)
+
+    constructor() {
+
+        this.instance.listen(() => {
+
+            const data = new URL(window.location.toString())
+
+                data.searchParams.values().next()
+
+        })
+
+        window.onpopstate = () => {
+            this.instance.set(window.location)
+        }
+    }
+
+    navigate(url: string) {
+
+        window.history.pushState(undefined, undefined, url)
+        this.instance.set(window.location)
+
+    }
+
+}
+
+const Utility = {
+
+    parseSearchParams(url: string) {
+
+        const obj = {}
+        const URLObj = new URL(url)
+
+        URLObj.searchParams.forEach((v, k) => {
+
+            obj[k] = v
+
+        })
+
+        return obj
+
+    },
+
+    defaultErrorPath() {
+
+        return Layout.Column({
+            style: {
+                padding: "10px",
+                gap: "5px",
+            },
+            children: [
+                Content.TextBox({
+                    style: {
+                        fontSize: "24px",
+                        fontWeight: "bold",
+                    },
+                    child: 
+                        Content.Text("404 : Route not found")
+                }),
+                Content.Text("If you wish a custom error route, provide an errorPath: Component")
+            ]
+        })
+
+    }
+
+}
+
 export const Router = {
 
-    Virtual({ routerState, routes }: { routerState: State<{ route: string, data: any }>, routes: { [key: string]: <T>(data: T) => Component } }) {
+    Virtual({ routerState, routes, errorPath = Utility.defaultErrorPath() }: { routerState: State<{ route: string, data: any }>, routes: { [key: string]: <T>(data: T) => Component }, errorPath?: Component }) {
 
         //the router state works as history.
         //we will just pick the last item from the stack and that's it!.
@@ -57,11 +126,21 @@ export const Router = {
 
             })
 
-        ])
+        ], errorPath)
 
     },
 
-    Browser() {
+    Browser({ routerState, routes, errorPath = Utility.defaultErrorPath() }: { routerState: State<Location>, routes: { [key: string]: <T>(data: T) => Component }, errorPath?: Component }) {
+
+        return $switch([routerState], [
+
+            ...Object.keys(routes).map(route => {
+
+                return $when(() => routerState.get().pathname == route, () => routes[route](Utility.parseSearchParams(routerState.get().toString())))
+
+            })
+
+        ], errorPath)
 
     }
 
